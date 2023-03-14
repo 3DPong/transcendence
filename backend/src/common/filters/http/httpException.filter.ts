3cb9780d7a092e05afe85633ad0aa00b45cbd69b
photogrammer <khsj15@khu.ac.kr>
2023-03-14 19:01:58 +0900

    feat: logger, global http exception filter 설정

---
+++ backend/src/common/filters/http/httpException.filter.ts
@@ -0,0 +1,23 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+/**
+ * Global HTTP exception filter
+ * 현재는 아무 기능 없습니다.
+ */
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  // private logger = new Logger();
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
