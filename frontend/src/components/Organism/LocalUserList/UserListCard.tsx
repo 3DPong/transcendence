de5ef71c761af8833b06f694ea4b020859f20884
kimminkyeu <minkyeki@student.42seoul.kr>
2023-03-16 03:38:35 +0900

    :truck: Fix: 한개 파일 2개로 분리 - (기존) UserListCard를 modecule에 넣고 GlobalUserList와 LocalUserList에서 사용 - (이후) Global, Local 각자의 디렉토리 안에 컴포넌트를 넣음 - (이유) Global에서 버튼 클릭시와 Local에서 버튼 클릭시 라우팅되는 경로가 다르기 때문...

--- frontend/src/components/Organism/LocalUserList/UserListCard.tsx
+++ frontend/src/components/Organism/LocalUserList/UserListCard.tsx
@@ -18,28 +18,70 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Skeleton from '@mui/material/Skeleton';
 import React from 'react';
-import { Tooltip } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
 
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Badge from '@mui/material/Badge';
 
+import { styled } from '@mui/system';
+import { createTheme } from '@mui/material/styles';
 import { user_t, userListData_t, eUserStatus } from '@/types/user';
 import EditIcon from '@mui/icons-material/Edit';
 import Divider from '@mui/material/Divider';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
+import { BorderOuter } from '@mui/icons-material';
+import ButtonLink from '@/components/Molecule/Link/ButtonLink';
 
-function UserActionMenu() {
+// https://stackoverflow.com/questions/42253277/react-router-v4-how-to-get-current-route
+import { useLocation } from 'react-router-dom'
+// https://stackoverflow.com/questions/29244731/how-to-manually-invoke-link-in-react-router
+import { useNavigate } from 'react-router-dom';
+import { circleOfConfusionPixelShader } from '@babylonjs/core/Shaders/circleOfConfusion.fragment';
+
+
+interface userActionMenuProps {
+    user : userListData_t
+}
+
+function UserActionMenu(props: userActionMenuProps) {
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+
+    // React Router useNavigate hook (프로필 보기 클릭시 이동)
+    const navigate = useNavigate();
+
+    // 프로필 보기 버튼
+    const handleProfileRoute = () => {
+        setAnchorEl(null);
+        navigate( `/friends/${props.user.profile.id}` );
+    }
+
+    // DM 보내기 버튼
+    const handleSendMessageRoute = () => {
+        setAnchorEl(null);
+        console.log("DM 보내기")
+        // ...
+    }
+
+    // 차단하기 버튼
+    const handleBlockRoute = () => {
+        setAnchorEl(null);
+        console.log("친구 차단")
+        // ...
+    }
+
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
     };
+
     const handleClose = () => {
-    setAnchorEl(null);
+        setAnchorEl(null);
     };
 
     return (
@@ -94,48 +136,76 @@ function UserActionMenu() {
                 transformOrigin={{ horizontal: "right", vertical: "top" }}
                 anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
             >
-                <MenuItem onClick={handleClose} disableRipple>
-                    <EditIcon />
+
+
+                <MenuItem onClick={handleProfileRoute} disableRipple>
+                    {/* <EditIcon /> */}
                     See profile
                 </MenuItem>
+
                 <Divider sx={{ my: 0.5 }} />
-                <MenuItem onClick={handleClose} disableRipple>
-                    <FileCopyIcon />
+
+                <MenuItem onClick={handleSendMessageRoute} disableRipple>
+                    {/* <FileCopyIcon /> */}
                     Send messege
                 </MenuItem>
+
                 <Divider sx={{ my: 0.5 }} />
-                <MenuItem onClick={handleClose} disableRipple>
-                    <ArchiveIcon />
+
+                <MenuItem onClick={handleBlockRoute} disableRipple>
+                    {/* <ArchiveIcon /> */}
                     Block user
                 </MenuItem>
+
+
             </Menu>
         </div>
     );
-
 }
 
 export interface UserCardProps {
     user: userListData_t
-    // imgSrc?: string,
-    // name?: string,
     isLoading?: boolean, // for skeleton
 };
 
 export default function UserListCard(props: UserCardProps) {
+    
+    /***********************************************
+     * [ How to Create styled Component ]          *
+     * https://mui.com/system/styled/#main-content *
+     *                                             *
+    ***********************************************/
+   
+    const isOnline = (status: eUserStatus) => {
+        return (status === eUserStatus.online);
+    }
+
     return (
         // 부모 flex container에 꽉 채우기 위한 용도 div
-        <div className="m-4 flex-1 max-w-full border border-green-700">
+        <div className="flex-1 border border-green-700">
 
             <div className=" w-auto flex items-center">
 
                 {/* (1) 프로필 사진 */}
                 { props.isLoading ? (
-                    <Skeleton animation="wave" variant='rectangular'>
-                        <Avatar variant='square' />
-                    </Skeleton>
-                ) : (
-                    <Avatar variant='square' alt={props.user.profile.name} src={props.user.profile.imgSrc} />
-                )
+                        <Skeleton animation="wave" variant='rectangular'>
+                            <Avatar variant='square' />
+                        </Skeleton>
+                    ) : (
+                        <Badge /* User [online/offline] 여부 표시 */
+                        sx={{ "& .MuiBadge-badge" : {
+                            backgroundColor: (isOnline(props.user.status)) ? ("lightgreen") : ("lightgray"),
+                            border: 1
+                        }}}
+                        badgeContent={1}
+                        variant="dot"
+                        anchorOrigin={{
+                            vertical: "top",
+                            horizontal: "left",
+                        }}>
+                            <Avatar variant='square' alt={props.user.profile.name} src={props.user.profile.imgSrc} />
+                        </Badge>
+                    )
                 }
 
                 {/* (2) 프로필 이름 */}
@@ -147,7 +217,7 @@ export default function UserListCard(props: UserCardProps) {
 
                 {/* 차단 버튼, DM 보내기 버튼.*/}
                 <div className="">
-                    <UserActionMenu />
+                    <UserActionMenu user={props.user}/>
                 </div>
 
             </div>
