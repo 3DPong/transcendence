450f86283c9681b5ee2c072ee3fda6b333a37df5
kimminkyeu <minkyeki@student.42seoul.kr>
2023-03-16 03:40:59 +0900

    :hammer: Fix : 위 변경사항에 따른 import 경로 변경 + 테스트 데이터 수정

--- frontend/src/components/Organism/GlobalUserList/UserList.tsx
+++ frontend/src/components/Organism/GlobalUserList/UserList.tsx
@@ -18,7 +18,7 @@
 import * as React from 'react';
 import { ListItemButton } from '@mui/material';
 import { ListItem } from '@mui/material';
-import UserListCard from '@/components/Molecule/UserListCard';
+import UserListCard from '@/components/Organism/GlobalUserList/UserListCard';
 import { Box } from '@mui/material';
 import { user_t, userListData_t, eUserStatus } from '@/types/user';
 import { FixedSizeList } from 'react-window';
@@ -29,14 +29,13 @@ const Row = (props: {index: number, style: React.CSSProperties, data: {users: Ar
     const { isLoading } = data;
 
     return (
-        // <ListItemButton style={style} key={index} divider={true}>
-        <ListItem style={style} key={index} divider={true}>
-            <UserListCard
-                user={user}
-                isLoading={isLoading}
-            />
+        <ListItem
+            style={style}
+            key={index}
+            divider={true}
+        >
+            <UserListCard user={user} isLoading={isLoading} />
         </ListItem>
-        // </ListItem>
     );
 }
 
@@ -47,7 +46,11 @@ export interface UserListProps {
 
 export default function VirtualizedUserList(props: UserListProps) {
 
-    if (!props.users) {
+    const LIST_HEIGHT = 400;
+    const ROW_WIDTH = "100%"
+    const ROW_HEIGHT = 70;
+
+    if (!props.users) { // on initial load
         return (<div></div>)
     }
 
@@ -64,9 +67,9 @@ export default function VirtualizedUserList(props: UserListProps) {
             {/* https://medium.datadriveninvestor.com/handling-lists-with-react-window-79c68a73c55a */}
             {/* https://mui.com/material-ui/react-list/ */}
             <FixedSizeList
-                height={400}
-                width="100%"
-                itemSize={60}
+                height={LIST_HEIGHT}
+                width={ROW_WIDTH}
+                itemSize={ROW_HEIGHT}
                 itemCount={ props.users.length }
                 overscanCount={5}
                 itemData={ { users : props.users, isLoading : props.isLoading } }
