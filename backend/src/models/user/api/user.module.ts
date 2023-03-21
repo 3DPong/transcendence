adc3c14ce9c0abbb209c27c07698a06e696d2057
Photogrammer <khsj15@khu.ac.kr>
2023-03-21 23:57:22 +0900

    chore: user_relation 으로 컨트롤러 라우팅 변경

--- backend/src/models/user/api/user.module.ts
+++ backend/src/models/user/api/user.module.ts
@@ -8,10 +8,11 @@ import { SessionStrategy } from '../../../common/guards/session/session.strategy
 import { UserCreationGuard } from '../../../common/guards/signup.guard.ts/userCreation.guard';
 import { UserCreationStrategy } from '../../../common/guards/signup.guard.ts/userCreation.strategy';
 import { UserRelationController } from './userRelation.controller';
+import { UserRelationService } from './services/userRelation.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, UserRelation])],
   controllers: [UserController, UserRelationController],
-  providers: [UserService, SessionGuard, SessionStrategy, UserCreationGuard, UserCreationStrategy],
+  providers: [UserService, UserRelationService, SessionGuard, SessionStrategy, UserCreationGuard, UserCreationStrategy],
 })
 export class UserModule {}
--- backend/src/models/user/api/userRelation.controller.ts
+++ backend/src/models/user/api/userRelation.controller.ts
@@ -4,7 +4,7 @@ import { UserRelationService } from './services/userRelation.service';
 import { GetSessionData } from '../../../common/decorators';
 import { GetUserRelationResDto, UpdateUserRelationResDto, UpdateUserRelationReqDto } from './dtos';
 
-@Controller('user/relation')
+@Controller('user_relation')
 export class UserRelationController {
   constructor(private userRelationService: UserRelationService) {}
 
@@ -17,12 +17,6 @@ export class UserRelationController {
     switch (relation) {
       case 'friend': {
         return await this.userRelationService.getUserAllRelation(sessionData.user_id);
-      }
-      case 'block': {
-        return await this.userRelationService.getUserFriendRelation(sessionData.user_id);
-      }
-      default: {
-        return await this.userRelationService.getUserBlockRelation(sessionData.user_id);
       }
     }
   }
