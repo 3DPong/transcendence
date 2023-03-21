123ffe1b02557fe13397b7aacbcca0b8643525b1
kdh <parktest0325@gmail.com>
2023-03-22 05:59:23 +0900

    :sparkles: feat : ChatDetail popper 정적폼 구현 완료
    popper를 아바타 bottom에 표시
    스크롤 이벤트발생시 popper close

--- frontend/src/components/Molecule/Chat/Detail/AvatarPopper.tsx
+++ frontend/src/components/Molecule/Chat/Detail/AvatarPopper.tsx
@@ -1,60 +1,109 @@
-import { FC } from "react";
-import { ClickAwayListener, MenuItem, MenuList, Paper } from '@mui/material';
+import { FC, useEffect } from "react";
+import { ClickAwayListener, MenuItem, MenuList, Paper, Popper } from '@mui/material';
 
 interface AvatarPopperProps {
-  isOpen: boolean;
+  anchorEl : HTMLElement | null;
   handleClose: () => void;
   targetId : number;
-  position: {
-    top: number;
-    left: number;
-  }
+  scrollY : number;
 };
 
-const AvatarPopper : FC<AvatarPopperProps> = ({isOpen, handleClose, targetId, position}) => {
+const AvatarPopper : FC<AvatarPopperProps> = ({anchorEl, handleClose, targetId, scrollY}) => {
+  const open = Boolean(anchorEl);
+  const isAdmin = true;
+  const isMuted = false;
+  const isBanned = false;
 
+  const menuItemStyles = {
+    fontSize: 'small',
+    padding: '4px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  };
+
+  useEffect(() => {
+    open && handleClose();
+  }, [scrollY]);
+
+  function handleProfileClick(id: number) {
+    console.log(id + " is profile");
+    handleClose();
+  }
   function handleBanClick(id: number) {
     console.log(id + " is bann");
     handleClose();
   }
-  function handleKickClick(id: number) {
-    console.log(id + " is kick");
+  function handleUnBanClick(id: number) {
+    console.log(id + " is Unbann");
     handleClose();
   }
   function handleMuteClick(id: number) {
     console.log(id + " is mute");
     handleClose();
   }
+  function handleUnMuteClick(id: number) {
+    console.log(id + " is Unmute");
+    handleClose();
+  }
+  function handleKickClick(id: number) {
+    console.log(id + " is kick");
+    handleClose();
+  }
+
+  const modifiers = [
+    {
+      name: 'offset',
+      options: {
+        offset: [12, 0],
+      },
+    },
+  ];
 
   return (
-    <>
-    { isOpen && (
-      <div
+      <Popper
+        open={open}
+        anchorEl={anchorEl}
         style={{
-          position: "fixed",
-          top: position.top,
-          left: position.left,
-          zIndex: 2
+          zIndex: 2,
         }}
+        placement="bottom-start"
+        modifiers={modifiers}
       >
         <ClickAwayListener onClickAway={handleClose} >
           <Paper>
-            <MenuList>
-              <MenuItem className="flex items-center p-2 hover:bg-gray-100" onClick={()=>{handleBanClick(targetId);}}>
-                Ban
+            <MenuList
+              disablePadding
+              className="border-gray-700 bg-slate-100 text-slate-800"
+            >
+              <MenuItem sx={menuItemStyles} onClick={()=>{handleProfileClick(targetId);}}>
+                See Profile
               </MenuItem>
-              <MenuItem className="flex items-center p-2 hover:bg-gray-100" onClick={()=>{handleKickClick(targetId);}}>
-                Kick
-              </MenuItem>
-              <MenuItem className="flex items-center p-2 hover:bg-gray-100" onClick={()=>{handleMuteClick(targetId);}}>
-                Mute
-              </MenuItem>
+              { isAdmin && [
+                  isMuted ? 
+                  <MenuItem key={1} sx={menuItemStyles} onClick={()=>{handleUnMuteClick(targetId);}}>
+                    UnMute
+                  </MenuItem> :
+                  <MenuItem key={2} sx={menuItemStyles} onClick={()=>{handleMuteClick(targetId);}}>
+                    Mute
+                  </MenuItem>,
+                  isBanned ?
+                  <MenuItem key={3} sx={menuItemStyles} onClick={()=>{handleUnBanClick(targetId);}}>
+                    UnBan
+                  </MenuItem> :
+                  <MenuItem key={4} sx={menuItemStyles} onClick={()=>{handleBanClick(targetId);}}>
+                    Ban
+                  </MenuItem>,
+                  <MenuItem key={5} sx={menuItemStyles} onClick={()=>{handleKickClick(targetId);}}>
+                    Kick
+                  </MenuItem>,
+                ]}
             </MenuList>
           </Paper>
         </ClickAwayListener>
-      </div>
-    )}
-    </>
+      </Popper>
   );
 };
 
