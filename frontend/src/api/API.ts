64d0af883ea2e7c765c6ea19ba91e1e3f8c13583
kimminkyeu <minkyeki@student.42seoul.kr>
2023-03-23 04:31:14 +0900

    :recycle: Refactor : API 정리

--- frontend/src/api/API.ts
+++ frontend/src/api/API.ts
@@ -1,7 +1,37 @@
 
 // 여기서 API 리스트들 묶어서 내보내기.
-// 사용할 땐 import * as 이름 from "@/api/API"
-export { changeUserRelation, RelationAction } from "./userRelation";
-export type { RelationFormat } from "./userRelation";
 
-export { getGlobalUsersListData, getFriendsListData } from "./userData";
+// (1) User Data
+export {
+
+    // GET
+    type GET_UserDataResponseFormat,
+    getUserDataById,
+
+    // POST
+    type POST_UserDataRequestFormat,
+    type POST_UserDataResponseFormat,
+    updateUserData,
+
+} from "@/api/user/userData";
+
+// (2) User Relation
+export {
+    type Relation,
+
+    // GET /user/search/{string}
+    type GET_GlobalSearchResponseFormat,
+    getUsersListBySearchString,
+
+    // GET /user_relation?query
+    GET_RelationType, 
+    type GET_RelationResponseFormat,
+    getUserRelationsList,
+
+    // PUT /user_relation
+    type PUT_RelationRequestFormat,
+    type PUT_RelationResponseFormat,
+    PUT_RelationActionType,
+    changeUserRelation,
+
+} from "@/api/user/userRelation";
