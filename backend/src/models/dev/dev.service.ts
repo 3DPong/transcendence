b1b5a62ad818be1d48403f553de572ab5aa09509
photogrammer <khsj15@khu.ac.kr>
2023-03-23 18:40:49 +0900

    fix: 존재하는 유저에 대해서 세션을 받을 수 있도록 처리

--- backend/src/models/dev/dev.service.ts
+++ backend/src/models/dev/dev.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { Request } from 'express';
 import { User } from '../user/entities';
 import { SessionService } from '../../common/session/session.service';
@@ -12,19 +12,43 @@ export class DevService {
     private sessionService: SessionService
   ) {}
 
-  async getSignInSession(req: Request, email: string, nickname: string) {
-    const user: User = new User();
-    user.email = email ? email : 'test@42.fr';
-    user.nickname = nickname ? nickname : 'tester42';
-    user.profile_url = 'https://example.com';
+  async getSignInSession(req: Request, email: string, nickname: string, userId) {
+    let user;
+    let existUser;
+    if (userId) {
+      existUser = await this.userRepository.findOne({
+        where: {
+          user_id: userId,
+        },
+      });
+    }
+    if (existUser) {
+      user = existUser;
+    } else {
+      user = new User();
+      user.email = email ? email : 'test@42.fr';
+      user.nickname = nickname ? nickname : 'tester42';
+      user.profile_url = 'https://example.com';
+    }
 
-    const saved = await this.userRepository.save(user);
-    this.sessionService.createSession(saved, req);
-    return {
-      status: 'SUCCESS',
-      user_id: saved.user_id,
-    };
+    try {
+      const saved = await this.userRepository.save(user);
+      this.sessionService.createSession(saved, req);
+      return {
+        status: 'SUCCESS',
+        user_id: saved.user_id,
+      };
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        if (error.message.includes('unique constraint') && error.message.includes('violates unique constraint')) {
+          throw new ConflictException('duplicate nickname');
+        }
+      } else {
+        throw new InternalServerErrorException('Internal server error');
+      }
+    }
   }
+
   async getSignUpSession(req: Request, email: string) {
     req.session.user_id = null;
     req.session.status = null;
