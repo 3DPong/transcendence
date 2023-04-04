a0fa2c5b19e3478277d8eb403ce50356ed77e7f8
kdh <parktest0325@gmail.com>
2023-04-05 03:33:55 +0900

    :truck: Fix: 유저리스트 UI 변경 #79

--- frontend/src/components/Molecule/Chat/Menu/MenuFooter.tsx
+++ frontend/src/components/Molecule/Chat/Menu/MenuFooter.tsx
@@ -24,7 +24,7 @@ const MenuFooter : FC<MenuFooterProps> = ({handleSetting, handleLeave, settingOp
       {
         isAdmin &&
         <button
-          className={`flex items-center text-gray-500 focus:outline-none
+          className={`flex items-center focus:outline-none
             ${settingOpen ? "hover:text-amber-700 text-amber-500" : "hover:text-gray-700 text-gray-500"}`}
           onClick={handleSetting}
         >
