97fa89e12c80d4e446e0e44626bb51199fb40f74
kdh <parktest0325@gmail.com>
2023-04-04 23:04:14 +0900

    :truck: Fix: 채팅리스트 owner 표시, DM 제목 변경 #79

--- frontend/src/components/Molecule/Chat/List/ChannelCard.tsx
+++ frontend/src/components/Molecule/Chat/List/ChannelCard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { Box, ListItemButton, Skeleton } from "@mui/material";
 import { ChannelType, Channel, defaultThumbnail } from "@/types/chat";
 import { Lock, LockOpen, Public, Sms } from "@mui/icons-material";
+import { RiVipCrown2Fill } from 'react-icons/ri';
 
 interface ChannelCardProps {
   channel: Channel;
@@ -42,23 +43,32 @@ const LoadedCard : FC<LoadedCardProps> = ({channel}) => {
   };
 
   return (
-    <>
-      <img
-        src={channel.thumbnail || defaultThumbnail}
-        alt="thumbnail"
-        className="w-12 h-12 rounded-full"
-      />
-      <div className="flex flex-col pl-2">
-        <span
-          className="text-md font-medium overflow-hidden whitespace-nowrap text-ellipsis"
-          style={{textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: 180}}>
-          {channel.title}
-        </span>
-        <span className="text-gray-500">
-          {getIcon(channel.type)}  {channel.type}
-        </span>
+  <>
+    <img
+      src={channel.thumbnail || defaultThumbnail}
+      alt="thumbnail"
+      className="w-12 h-12 rounded-full"
+    />
+    <div className="flex flex-col pl-2 pr-2 w-full">
+      <span
+        className="text-md font-medium overflow-hidden whitespace-nowrap text-ellipsis"
+        style={{textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: 180}}>
+        {channel.type === "dm" ? channel.owner.nickname + "님 과의 DM" : channel.title}
+      </span>
+      <div className="text-gray-500 flex items-center w-full">
+        <div style={{width: '50%'}}>
+          {getIcon(channel.type)} {channel.type}
+        </div>
+        <div style={{width: '50%'}} className="flex items-center half-width justify-start">
+          <RiVipCrown2Fill size="1.2em" color="Blue" style={{paddingRight: '4px'}}/>
+          <span
+            style={{overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: 70}}>
+            {channel.owner.nickname}
+          </span>
+        </div>
       </div>
-    </>
+    </div>
+  </>
   );
 };
 
