c7c1ff71ad79958fa5cbcd9267ad50004c425c7a
kimminkyeu <minkyeki@student.42seoul.kr>
2023-04-05 22:48:36 +0900

    API 연결 완료

--- frontend/src/context/ErrorContext.tsx
+++ frontend/src/context/ErrorContext.tsx
@@ -1,13 +1,15 @@
 import { createContext, useContext, useState, ReactNode } from 'react';
 
+export type handleErrorFunction = (title:string | null, message: string | null, redirectUrl?: string | null) => void;
+
 interface ErrorContextValue {
   errorTitle: string | null;
   errorMessage: string | null;
-  action: (() => void) | null;
-  handleError: (title:string | null, message: string | null, action?: ()=>void) => void;
+  // action: (() => void) | null;
+  redirectUrl: string | null;
 
+  handleError: handleErrorFunction;
 }
-export type handleErrorFunction = (title:string | null, message: string | null, action?: ()=>void) => void;
 
 const ErrorContext = createContext<ErrorContextValue | null>(null);
 
@@ -27,16 +29,16 @@ interface ErrorProviderProps {
 export function ErrorProvider({ children }: ErrorProviderProps): JSX.Element {
   const [errorTitle, setErrorTitle] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [action, setAction] = useState<(()=>void) | null>(null);
+  const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
 
-  function handleError(title: string | null, message: string | null, action?: ()=>void ) {
+  function handleError(title: string | null, message: string | null, redirectUrl?: string | null) {
     setErrorTitle(title);
     setErrorMessage(message);
-    action && setAction(action); // if param exists, set callback state.
+    redirectUrl && setRedirectUrl(redirectUrl); // if param exists, set callback state.
   }
 
   return (
-    <ErrorContext.Provider value={{ errorTitle, errorMessage, action, handleError }}>
+    <ErrorContext.Provider value={{ errorTitle, errorMessage, redirectUrl, handleError }}>
       {children}
     </ErrorContext.Provider>
   );
