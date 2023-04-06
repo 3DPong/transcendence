02df175343d673457e3cbda9b8ac9c0ecb991043
Photogrammer <khsj15@khu.ac.kr>
2023-04-06 20:20:24 +0900

    feat: connect, disconnect 시에 변경 정보 주변으로 전송

--- backend/src/models/userStatusSocket/socket/userStatusSocket.module.ts
+++ backend/src/models/userStatusSocket/socket/userStatusSocket.module.ts
@@ -3,9 +3,10 @@ import { UserStatusSocketService } from './services';
 import { UserStatusSocketGateway } from './userStatusSocket.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../user/entities';
+import { NotifierModule } from '../../notifier/notifier.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), NotifierModule],
   providers: [UserStatusSocketGateway, UserStatusSocketService],
 })
 export class UserStatusSocketModule {}
