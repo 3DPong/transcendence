a88b82a4e72cc64eb229348879b412b38d265d06
schoe <schoe@student.42seoul.kr>
2023-04-11 19:33:29 +0900

    game api변경(data 이름 수정, start 분리)

--- backend/src/models/game/gameData/OnSceneData.ts
+++ backend/src/models/game/gameData/OnSceneData.ts
@@ -1,14 +1,14 @@
 import { PlayerLocation } from "../enum/GameEnum";
-import { ObjectData } from "../gameData/ObjectData";
+import { ObjectData } from "./ObjectData";
 
-interface MatchStartInterface {
+interface OnSceneDataInterface {
   gameId : string; // 프론트에서 항상 데이터에 담아서 보내줘야 함.
 	playerLocation : PlayerLocation; // 플레이어가 왼쪽인지 오른쪽인지 알려줌
 	enemyUserId : number; // 상대방 유저 id
 	sceneData : ObjectData[]; // Scene을 구성하는 물체들의 정보 
 }
 
-export class MatchStartData implements MatchStartInterface {
+export class OnSceneData implements OnSceneDataInterface {
   public gameId : string;
 	public playerLocation : PlayerLocation; 
 	public enemyUserId : number;
--- backend/src/models/game/socket/game.socket.adapter.ts
+++
@@ -1,7 +0,0 @@
-import { IoAdapter } from "@nestjs/platform-socket.io";
-export class GameSocketIoAdapter extends IoAdapter {
-  createIOServer(port: number, options?: any) {
-    const server = super.createIOServer(port, options);
-    return server;
-  }
-}
