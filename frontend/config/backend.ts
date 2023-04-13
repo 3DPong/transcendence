fe0cfcb127855d93476bb163ddeab9432786701d
kimminkyeu <minkyeki@student.42seoul.kr>
2023-04-13 21:04:53 +0900

    [TEMP!] : 다른 클러스터 컴퓨터와 대결하기 위한 localhost 임시 설정 (추후 복수 필요)

--- frontend/config/backend.ts
+++ frontend/config/backend.ts
@@ -1,5 +1,10 @@
+
 export const API_PORT = 3000;
-export const API_URL = `http://localhost:${API_PORT}`;
+// export const API_URL = `http://localhost:${API_PORT}`;
+export const API_URL = `http://10.30.5.9:${API_PORT}`;
 
 export const SOCKET_PORT = 3000;
-export const SOCKET_URL = `http://localhost:${SOCKET_PORT}`;
+// export const SOCKET_URL = `http://localhost:${SOCKET_PORT}`;
+export const SOCKET_URL = `http://10.30.5.9:${SOCKET_PORT}`;
+
+// TODO: LocalHost를 추후에 환경변수로 받아와서 ip 설정해주기 (그래야 다른 컴퓨터랑 대결 가능함)
