ced69bbfacfc4498df375618564b677431ffe95c
Photogrammer <khsj15@khu.ac.kr>
2023-04-18 18:50:29 +0900

    fix: user creation cookie에서 토큰 가져오도록 변경

--- backend/src/common/guards/userCreation/userCreation.strategy.ts
+++ backend/src/common/guards/userCreation/userCreation.strategy.ts
@@ -4,12 +4,17 @@ import { TokenStatusEnum } from '../../enums/tokenStatusEnum';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtConfigService } from '../../../config/jwt/config.service';
 import { JwtPayloadInterface } from '../../interfaces/JwtUser.interface';
+import { Request } from 'express';
 
 @Injectable()
 export class UserCreationStrategy extends PassportStrategy(Strategy, 'JwtCreationStrategy') {
   constructor(private jwtConfigService: JwtConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request): string => {
+          return req?.cookies?.Authentication;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: jwtConfigService.secret,
     });
