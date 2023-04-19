d1a0a8d68964204fcb1e99ee840ed5b08cb2a6ea
Photogrammer <khsj15@khu.ac.kr>
2023-04-19 20:08:20 +0900

    refactor: notify user_status interface 정의

--- backend/src/models/user/api/services/user.service.ts
+++ backend/src/models/user/api/services/user.service.ts
@@ -13,10 +13,14 @@ import { GetUserSettingResDto } from '../dtos/getUserSettingRes.dto';
 import { JwtPayloadInterface } from '../../../../common/interfaces/JwtUser.interface';
 import { VerifyNicknameResponseDto } from '../dtos/verifyNickname.dto';
 import { SearchedUser, SearchUserResDto } from '../dtos/searchUserRes.dto';
+import { Notifier } from '../../../notifier/services/notifier.class';
+import { TopicEnum } from '../../../notifier/enums/topic.enum';
+import { ChannelEnum } from '../../../notifier/enums/channel.enum';
+import { UserUpdateDto } from '../../../../common/interfaces/userUpdate.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  constructor(@InjectRepository(User) private userRepository: Repository<User>, private readonly notifier: Notifier) {}
 
   async getUser(userId: number): Promise<GetUserResDto> {
     const user: GetUserResDto = await this.userRepository.findOne({
@@ -73,7 +77,16 @@ export class UserService {
     }
 
     try {
-      await this.userRepository.update({ user_id: userId }, payload);
+      const res = await this.userRepository.update({ user_id: userId }, payload);
+      if (res.affected === 0) throw new InternalServerErrorException('server error'); // check update is success
+      // notify to all users
+      const userUpdated: UserUpdateDto = {
+        user_id: userId,
+        nickname: payload.nickname ? payload.nickname : userToUpdate.nickname,
+        profile_url: payload.profile_url ? payload.profile_url : userToUpdate.profile_url,
+        status: userToUpdate.status,
+      };
+      await this.notifier.notify(userId, 'user_status', userUpdated, TopicEnum.USER, ChannelEnum.ALL, 0);
     } catch (error) {
       if (error instanceof QueryFailedError) {
         if (error.message.includes('unique constraint') && error.message.includes('violates unique constraint')) {
