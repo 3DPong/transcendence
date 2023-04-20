f011d3ee7e108dcef2730218307f03d7faa5622b
schoe <schoe@student.42seoul.kr>
2023-04-20 17:39:38 +0900

    공 초기 속도, 방향 fix

--- backend/src/models/game/gameData/InGameData.ts
+++ backend/src/models/game/gameData/InGameData.ts
@@ -7,7 +7,7 @@ export class InGameData{
   constructor (id :string, name :string){
     this.id = id;
     this.name = name;
-    this.pause = false;
+    this.pause = true;
     this.player1_score = 0;
     this.player2_score = 0;
   }
